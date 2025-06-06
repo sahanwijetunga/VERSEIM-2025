bacc56d844faa523c73442ea32f42ac4b97336cd
George McNinch <gmcninch@gmail.com>
2025-06-06 15:57:52 -0400

    convert

--- VERSEIM2025/george/convert.lean
+++ VERSEIM2025/george/convert.lean
@@ -11,5 +11,13 @@ example {x:G} : -x ∈ B →  x ∈ B := by
   · exact Eq.symm (neg_neg x)
   · exact inferInstance -- infer NegMemClass   (not at all sure why this doesn't happend automagically)
 
+-- example: -x ∈ B → x ∈ B := by
+--   intro h
+--   apply neg_mem
+--   sorry -- convert goal into showing - - x ∈ B
 
-
+example: -x ∈ B → x ∈ B := by
+  intro h
+  rw [ ← neg_neg x ]
+  apply neg_mem
+  exact h
