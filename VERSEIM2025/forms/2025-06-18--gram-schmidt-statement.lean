a3419fbc316344bdc2b3d0afda19b28f65e0ab91
George McNinch <gmcninch@gmail.com>
2025-06-23 09:52:26 -0400

    defn ind

--- VERSEIM2025/forms/2025-06-18--gram-schmidt-statement.lean
+++ VERSEIM2025/forms/2025-06-18--gram-schmidt-statement.lean
@@ -27,16 +27,34 @@ def orthog_by_gs {V:Type} [AddCommGroup V] [Module ℝ V]
    | Nat.succ m => by sorry
 
 
-def restrict {m:ℕ} (f:Fin (m+1) → ℕ) : Fin m → ℕ :=
+def restrict {X:Type} {m:ℕ} (f:Fin (m+1) → X) : Fin m → X :=
   fun i => f i.castSucc 
 
 
-def extend {m:ℕ} (f:Fin m → ℕ) (x :ℕ) : Fin (m+1) → ℕ :=
+def extend {X:Type} {m:ℕ} (f:Fin m → X) (x :X) : Fin (m+1) → X :=
   fun i =>
   if h:i ≠ Fin.last m then f (i.castPred h) else x
 
 #eval restrict ![2,3,4,5,8]
 
 #eval extend ![1,1,1] 2
-  
-  
+
+-- let's write a function which takes as argument
+-- a vector-values function w and returns the function
+-- whose value at i is `w i` minus the sum of all the `w j` for `j < i`.
+
+-- we can write this "recursively" as follows:
+
+def construct {V:Type} [AddCommGroup V] [Module ℚ V] {m:ℕ} (w:Fin (m+1) → V) 
+  : Fin (m+1) → V :=  
+  match m with
+   | 0 => w
+   | m+1 => extend (construct (restrict w)) (w (Fin.last (m+1)) - (∑ j : Fin (m+1), w j.castSucc))
+
+
+def sbv (n:ℕ) : Fin n → Fin n → ℚ := by
+  intro i
+  exact Pi.single i (1:ℚ)
+
+
+#eval construct (sbv 5)
