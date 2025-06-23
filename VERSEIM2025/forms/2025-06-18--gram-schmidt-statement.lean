ecfa29706ac63f16d4f9b2d1c21b0d207656d967
George McNinch <gmcninch@gmail.com>
2025-06-23 08:31:45 -0400

    gs

--- VERSEIM2025/forms/2025-06-18--gram-schmidt-statement.lean
+++ VERSEIM2025/forms/2025-06-18--gram-schmidt-statement.lean
@@ -12,19 +12,31 @@ import Mathlib.Tactic
 -- hence determines a basis, and then check that this basis satisfies
 -- `OrthogBasis`
 
-def PosDef (V : Type) [AddCommGroup V] [Module ℝ V] (β:V →ₗ[ℝ] V →ₗ[ℝ] ℝ) : Prop :=
+def PosDef {V : Type} [AddCommGroup V] [Module ℝ V] (β:V →ₗ[ℝ] V →ₗ[ℝ] ℝ) : Prop :=
   ∀ x : V, β x x ≥ 0 ∧ x ≠ 0 → β x x > 0
 
-def OrthogBasis (V : Type) [AddCommGroup V] [ Module ℝ V] (β:V →ₗ[ℝ] V →ₗ[ℝ] ℝ)
+def OrthogBasis {V : Type} [AddCommGroup V] [ Module ℝ V ] (β:V →ₗ[ℝ] V →ₗ[ℝ] ℝ)
   (c:Basis I ℝ V) : Prop := ∀ (i j : I), i ≠ j → β (c i) (c j) = 0
 
-def orthogonalize_by_gram_schmidt {V:ℕ → Type} [∀ m:ℕ , AddCommGroup (V m)] 
-  [∀ m:ℕ, Module ℝ (V m)] (β:(m:ℕ) → V m →ₗ[ℝ] V m →ₗ[ℝ] ℝ) 
-  (hp: ∀ m, PosDef (V m) (β m))
-  (b:(m:ℕ) → Basis (Fin m) ℝ (V m)) 
-  (n:ℕ) :
-  Fin n → V n := by
-  induction (n:ℕ) with 
-  | zero => exact 0
-  | succ => exact fun i => 
-    if i ≤ n 
+def orthog_by_gs {V:Type} [AddCommGroup V] [Module ℝ V] 
+  (β:V →ₗ[ℝ] V →ₗ[ℝ] ℝ) (hp : PosDef β)
+  {m:ℕ}
+  (b:Fin m → V) (hb : LinearIndependent ℝ b)
+  : Fin m → V := match m with 
+   | Nat.zero => b
+   | Nat.succ m => by sorry
+
+
+def restrict {m:ℕ} (f:Fin (m+1) → ℕ) : Fin m → ℕ :=
+  fun i => f i.castSucc 
+
+
+def extend {m:ℕ} (f:Fin m → ℕ) (x :ℕ) : Fin (m+1) → ℕ :=
+  fun i =>
+  if h:i ≠ Fin.last m then f (i.castPred h) else x
+
+#eval restrict ![2,3,4,5,8]
+
+#eval extend ![1,1,1] 2
+  
+  
