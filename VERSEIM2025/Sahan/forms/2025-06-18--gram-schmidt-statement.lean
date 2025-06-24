8f2658cebd93773453775a2f466c4b948ee3c1ce
sahanwijetunga <99368343+sahanwijetunga@users.noreply.github.com>
2025-06-24 07:25:48 -0700

    more code

--- VERSEIM2025/Sahan/forms/2025-06-18--gram-schmidt-statement.lean
+++ VERSEIM2025/Sahan/forms/2025-06-18--gram-schmidt-statement.lean
@@ -29,21 +29,19 @@ lemma posDef_is_Def {V : Type} [AddCommGroup V] [Module ℝ V] (β:V →ₗ[ℝ]
   apply (h x).right
   assumption
 
-def OrthogBasis {V : Type} [AddCommGroup V] [ Module ℝ V ] (β:V →ₗ[ℝ] V →ₗ[ℝ] ℝ)
-  (c:Basis I ℝ V) : Prop := ∀ (i j : I), i ≠ j → β (c i) (c j) = 0
 
 def OrthogPred {V : Type} [AddCommGroup V] [ Module ℝ V ] (β:V →ₗ[ℝ] V →ₗ[ℝ] ℝ)
   (c: I → V) : Prop := ∀ (i j : I), i ≠ j → β (c i) (c j) = 0
 
+
+def OrthogBasis {V : Type} [AddCommGroup V] [ Module ℝ V ] (β:V →ₗ[ℝ] V →ₗ[ℝ] ℝ)
+  (c:Basis I ℝ V) : Prop := OrthogPred β c
 
 @[ext]
 structure OrthogNonZero {V : Type} [AddCommGroup V] [ Module ℝ V ] (β:V →ₗ[ℝ] V →ₗ[ℝ] ℝ) (n: ℕ) where
   toFun: Fin n → V
   orthog: OrthogPred β toFun
   non_zero: (i: Fin n) → toFun i ≠ 0
-
-#check Fin 0
-
 
 def restrict {X:Type} {m:ℕ} (f:Fin (m+1) → X) : Fin m → X :=
   fun i => f i.castSucc
@@ -69,6 +67,12 @@ example {V : Type} [AddCommGroup V] [ Module ℝ V ] (β:V →ₗ[ℝ] V →ₗ[
 lemma not_needed_sum_one_nonzero_element {n: ℕ} (l: Fin n → ℝ) (i: Fin n) (h: (j: Fin n) → j ≠ i → l j =0):
   l i = ∑ j, l j := by exact Eq.symm (Fintype.sum_eq_single i h)
 
+lemma restrict_linear_independent {V:Type} [AddCommGroup V] [Module ℝ V]
+  {n:ℕ}  (b:Fin (n+1) → V) (hb : LinearIndependent ℝ b) : LinearIndependent ℝ (restrict b) := by
+    unfold restrict
+    apply LinearIndependent.comp hb Fin.castSucc
+    exact Fin.castSucc_injective n
+
 def orthog_by_gs {V:Type} [AddCommGroup V] [Module ℝ V]
   (β:V →ₗ[ℝ] V →ₗ[ℝ] ℝ) (hp : PosDef β) (hs : Symm β)
   {n:ℕ}
@@ -86,7 +90,7 @@ def orthog_by_gs {V:Type} [AddCommGroup V] [Module ℝ V]
    | Nat.succ n => by
     have hnlessnsucc : n<n+1 := by norm_num
     let b' := restrict b
-    have b'_linear_independent: LinearIndependent ℝ b' := by sorry
+    have b'_linear_independent: LinearIndependent ℝ b' := restrict_linear_independent b hb
     let c' := orthog_by_gs β hp hs b' b'_linear_independent
 
     let b_last := b ⟨n, hnlessnsucc⟩
@@ -95,7 +99,6 @@ def orthog_by_gs {V:Type} [AddCommGroup V] [Module ℝ V]
 
     have c_orthog_rest_last: (i: Fin n) → β (c'.toFun i) c_last=0 := by
       intro i
-      unfold c_last
       rw[map_sub]
       suffices β (c'.toFun i) (b_last) = β (c'.toFun i) (∑ j, proj β (c'.toFun j) b_last) from ?_
       . rw[this, sub_self]
@@ -112,7 +115,120 @@ def orthog_by_gs {V:Type} [AddCommGroup V] [Module ℝ V]
       rw[Fintype.sum_eq_single i orthog_i_j]
       rw[proj_nice' β _ _ ((posDef_is_Def β hp) (c'.toFun i) (c'.non_zero i))]
       apply hs
+
+    let c : Fin (n+1) → V := fun ⟨m, h⟩  =>
+      if h' : m ≠ n then c'.toFun ⟨m, Nat.lt_of_le_of_ne (Nat.le_of_lt_succ h) h'⟩
+        else c_last
+
+    have hc_last: c_last = (c ⟨n, hnlessnsucc⟩) := by
+      simp[c]
+    have c_orthog: OrthogPred β c := by
+
+
+      have ind_neq_n (ind: Fin (n+1)) (h: ind ≠ ⟨n,hnlessnsucc⟩): ↑ind ≠ n := by
+        show ↑ind ≠ (↑(⟨ n,hnlessnsucc⟩: Fin (n+1)): ℕ)
+        have: ↑(⟨ n,hnlessnsucc⟩: Fin (n+1)) = n := rfl
+        intro h'
+        exact h (Fin.ext_iff.mpr h')
+
+      have ind_lt_n (ind: Fin (n+1)) (h: ind ≠ ⟨n,hnlessnsucc⟩): ↑ind < n := by
+        suffices ↑ind ≤ n ∧ ↑ind ≠ n from ?_
+        . have ⟨h₀, h₁⟩ := this
+          exact Nat.lt_of_le_of_ne h₀ h₁
+        constructor
+        . exact Nat.le_of_lt_succ ind.is_lt
+        . exact ind_neq_n ind h
+
+
+      have hc_eq_c'_on_ind_neq_n (ind: Fin (n+1)) (h: ind ≠ ⟨n,hnlessnsucc⟩): c ind = c'.toFun ⟨↑ind,ind_lt_n ind h⟩ := by
+        have hj_neq_n: ↑ind ≠ n := by exact Nat.ne_of_lt (ind_lt_n ind h)
+        simp only [c]
+        rw[dif_pos ]
+        exact hj_neq_n
+
+      have h_one_is_n (i j: Fin (n+1)) (hjn: i ≠ j): (i=⟨n,hnlessnsucc⟩) → β (c i) (c j) = 0 := by
+        intro h'
+        rw[h', hs, <- hc_last]
+
+        let j': ℕ := ↑j
+        have hj': j'<(n+1) := j.is_lt
+        let i': ℕ := ↑i
+        have hi': i'<(n+1) := i.is_lt
+        have hi'n_eq: i'=n := by exact Fin.mk.inj_iff.mp h'
+
+        have hi'n_neq: i'≠ j' := Fin.val_ne_iff.mpr hjn
+
+        have hj_neq_n: j ≠ ⟨n,hnlessnsucc⟩ := by
+          rw[<- h']
+          symm
+          exact Fin.val_ne_iff.mp hi'n_neq
+
+        have j_ineq: j'<n := ind_lt_n j hj_neq_n
+
+        rw[hc_eq_c'_on_ind_neq_n]
+        apply c_orthog_rest_last
+        exact hj_neq_n
+
+      have h_none_is_n (i j: Fin (n+1)) (h: i ≠ j): (i≠ ⟨n,hnlessnsucc⟩) → (j ≠ ⟨n,hnlessnsucc⟩) → β (c i) (c j) = 0 := by
+        intro h₁ h₂
+        let j': ℕ := ↑j
+        have hj': j'<(n+1) := j.is_lt
+        let i': ℕ := ↑i
+        have hi': i'<(n+1) := i.is_lt
+
+        have hi'n_neq: i'≠ j' := by
+          intro h''
+          have: i=j := by
+            suffices ⟨i', hi'⟩ =(⟨j', hj'⟩ : Fin (n+1)) from ?_
+            . tauto
+            rw[Fin.mk.inj_iff]
+            exact h''
+          apply h this
+
+        have hi'_neq_n : i' ≠ n := ind_neq_n i h₁
+        have hj'_neq_n : j' ≠ n := ind_neq_n j h₂
+
+        have hi'_lt_n: i'<n := ind_lt_n i h₁
+        have hj'_lt_n: j'<n := ind_lt_n j h₂
+
+        let i_new: Fin n := ⟨i', hi'_lt_n⟩
+        let j_new: Fin n := ⟨j', hj'_lt_n⟩
+        rw[hc_eq_c'_on_ind_neq_n,hc_eq_c'_on_ind_neq_n]
+        apply c'.orthog
+        intro hcontr
+        rw[Fin.mk.inj_iff] at hcontr
+        . exact hi'n_neq hcontr
+        . exact h₁
+        . exact h₂
+
+      intro i j h
+      let h1:= (i ≠ ⟨n,hnlessnsucc⟩ ) ∧ (j ≠ ⟨n,hnlessnsucc⟩)
+      let h2:= (i = ⟨n,hnlessnsucc⟩ )
+      let h3:= (j = ⟨n,hnlessnsucc⟩)
+      have: h1 ∨ h2 ∨ h3 := by
+        rcases Classical.em (i=⟨ n,hnlessnsucc⟩) with h₁ | h₁
+        . right; left; exact h₁
+        . rcases Classical.em (j=⟨ n,hnlessnsucc⟩) with h₃ | h₄
+          . right; right; exact h₃
+          . left; exact ⟨ h₁, h₄⟩
+
+      rcases this with h1' | (h2' | h3')
+      . apply h_none_is_n i j h h1'.left h1'.right
+      . apply h_one_is_n i j h h2'
+      . rw[hs (c i) (c j)]
+        apply h_one_is_n j i h.symm h3'
+
+    /-
+    TODO: The only thing left to do is prove c_last ≠ 0. This will require showing that
+      span is preserved by orthog_by_gs (so add more to OrthogNonZero)
+
+    Once proof completed refactor to avoid using a class, and just have a definition of
+      c: Fin n → V given b: Fin n → V
+    then create a theorem that proves c is linearly independent and orthogonal if b is linearly independent.
+    -/
     sorry
+
+
 
 #eval restrict ![2,(3: ℚ ),(4: ℕ),5,8]
 
@@ -123,6 +239,8 @@ def extend {X:Type} {m:ℕ} (f:Fin m → X) (x :X) : Fin (m+1) → X :=
 #eval restrict ![2,3,4,5,8]
 
 #eval extend ![1,1,1] 2
+
+
 
 -- let's write a function which takes as argument
 -- a vector-values function w and returns the function
