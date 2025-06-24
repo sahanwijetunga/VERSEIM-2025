452823f1f6ef4d099830febb56b107e98d674ce3
George McNinch <gmcninch@gmail.com>
2025-06-24 12:37:32 -0400

    update minted example

--- documents/auto/using-minted-example.el
+++ documents/auto/using-minted-example.el
@@ -6,13 +6,33 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("graphicx" "") ("minted" "") ("newunicodechar" "")))
+                     '(("fontspec" "") ("minted" "") ("xcolor" "svgnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbatimWrite")
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbEnv")
+   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbatimOut")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "Verb")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "Verb*")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "EscVerb")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "EscVerb*")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb*")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb")
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art10"
-    "graphicx"
+    "fontspec"
     "minted"
-    "newunicodechar"))
+    "xcolor")
+   (LaTeX-add-minted-newmints
+    '("lean" "lean4" "inline")
+    '("leancode" "lean4" "ed")))
  :latex)
 
