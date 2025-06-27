4f5db5e24301d7c752a504ac18e2306f59496189
George McNinch <gmcninch@gmail.com>
2025-06-27 08:23:17 -0400

    upd

--- VERSEIM2025/disj.lean
+++ VERSEIM2025/disj.lean
@@ -3,65 +3,29 @@ import Mathlib.Tactic
 
 import Mathlib.Data.FinEnum
 
-#check FinEnum
-
 example (n m :ℕ) : FinEnum ((Fin n) ⊕ (Fin m)) := inferInstance
 
-#check FinEnum.card
-#eval FinEnum.card ((Fin 15) ⊕ (Fin 3))
+lemma card_eq_finenum_card (X:Type) [FinEnum X]  : Nat.card X = FinEnum.card X := by 
+  rw [ Nat.card_eq_fintype_card ]
+  exact Eq.symm FinEnum.card_eq_fintypeCard 
 
-
-#check @FinEnum.equiv ((Fin 15) ⊕ (Fin 3))
-
-
-example : (Fin 3) ⊕ (Fin 5) ≃ Fin 8 := 
-  FinEnum.equiv
-
-example : Nat.card (Fin 5) = FinEnum.card (Fin 5) := by simp
-
-lemma replace (X:Type) [FinEnum X]  : Nat.card X = FinEnum.card X := by 
-  refine Eq.symm (Nat.eq_of_beq_eq_true ?_) 
-
-
-
-example (n m : ℕ) : FinEnum.card ((Fin n) ⊕ (Fin m)) = n + m := by 
+lemma size_of_sum (n m : ℕ) : FinEnum.card ((Fin n) ⊕ (Fin m)) = n + m := by 
   have hn : n = FinEnum.card (Fin n) := Eq.symm FinEnum.card_fin 
   have hm : m = FinEnum.card (Fin m) := Eq.symm FinEnum.card_fin 
+  
+  suffices : FinEnum.card (Fin n) + FinEnum.card (Fin m) = FinEnum.card (Fin n ⊕ Fin m) := by sorry
+
   apply Eq.symm
   nth_rw 1 [ hn , hm ]
   apply Eq.symm
-  rw [ <-replace n, <-replace m ]
-  refine Nat.card_sum (α:= Fin n) (β := Fin m)
+  rw [ ← card_eq_finenum_card (Fin n), 
+       ← card_eq_finenum_card (Fin m), 
+       ← card_eq_finenum_card ((Fin n) ⊕ (Fin m)) ]
+  apply Nat.card_sum 
   
-  
-lemma f : FinEnum.card (Fin n) = n := by simp
-
-#print f
-
+ 
 example (n m : ℕ) : (Fin n) ⊕ (Fin m) ≃ Fin (n+m)  := by
-   have : n+m = FinEnum.card ((Fin n) ⊕ (Fin m)) := by 
+   rw [ ← size_of_sum]
+   exact FinEnum.equiv
     
 
-
-open Sum
-def fin_disjoint_fin_equiv_fin (n m: ℕ) : (Fin n) ⊕ (Fin m) ≃ Fin (n+m) where
-  toFun := fun i => 
-    match i with
-    | inl x => Fin.castAdd m x
-    | inr x => by
-        rw [ add_comm ] 
-        exact Fin.castAdd n x 
-  invFun := by 
-    rintro ⟨i,_⟩
-    if h : i < n then 
-       have : NeZero n := NeZero.mk (by linarith)
-       exact inl (Fin.ofNat n i)
-    else
-       have : NeZero m := NeZero.mk (by linarith)
-       exact inr (Fin.ofNat m (n-i))
-  left_inv := by sorry
-  right_inv := by sorry
-
-
-
--- 
