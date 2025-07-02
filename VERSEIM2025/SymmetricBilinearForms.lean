25a198201c8bf7d6fcfe12238a4bcd01886bbb50
sahanwijetunga <99368343+sahanwijetunga@users.noreply.github.com>
2025-07-02 14:04:11 -0700

    Update SymmetricBilinearForms.lean

--- VERSEIM2025/SymmetricBilinearForms.lean
+++ VERSEIM2025/SymmetricBilinearForms.lean
@@ -9,9 +9,10 @@ VERSEIM-2025 REU @ Tufts University
 
 import Mathlib.Tactic
 import Mathlib.LinearAlgebra.BilinearForm.Orthogonal
+import Mathlib.LinearAlgebra.QuadraticForm.Basic
 import VERSEIM2025.Sahan.BilinearForms
 
-/- Sahan: The main results in this file only hold for symmetric forms. -/
+/- Sahan: The main results in this file should hold only for symmetric forms. -/
 
 
 variable {k V: Type*} [AddCommGroup V][Field k][Module k V]
@@ -22,6 +23,6 @@ open LinearMap (BilinForm)
 -- Main result: symmetric bilinear form has orthogonal basis, from Mathlib
 example {V : Type u} {K : Type v}
    [Field K] [AddCommGroup V] [Module K V] [FiniteDimensional K V]
-   [hK : Invertible 2] {B : LinearMap.BilinForm K V} (hB₂ : IsSymm B) :
+   [hK : Invertible (2: K)] {B : LinearMap.BilinForm K V} (hB₂ : IsSymm B) :
     ∃ (v : Basis (Fin (Module.finrank K V)) K V), IsOrthoᵢ B ⇑v := by
-      exact LinearMap.BilinForm.exists_orthogonal_basis
+      exact LinearMap.BilinForm.exists_orthogonal_basis hB₂
