cb8db2883f3493e6ec5cfba76adc34901bce24fb
sahanwijetunga <99368343+sahanwijetunga@users.noreply.github.com>
2025-07-12 12:42:22 -0700

    ..

---
+++ VERSEIM2025/Forms/RationalFunctionFields/OverleafDoc.md
@@ -0,0 +1,1 @@
+See [here](https://www.overleaf.com/read/pmxzpttgvrdt#a21532) for link to overleaf doc. 
--- VERSEIM2025/Sahan/temp.lean
+++ VERSEIM2025/Sahan/temp.lean
@@ -1,6 +1,34 @@
 import Mathlib.Tactic
-import Mathlib.LinearAlgebra.QuadraticForm.Basic
-import Mathlib.LinearAlgebra.BilinearForm.Orthogonal
 
+open Real
 
-example (a b: Fin 1): a=b := sorry
+-- We tell `grind` that whenever it sees `cos x` or `sin x`,
+-- it should add `(cos x)^2 + (sin x)^2 = 1` to the whiteboard.
+grind_pattern cos_sq_add_sin_sq => sin x
+
+-- Here `grind` notices that `(cos x)^2 + (sin x)^2 = 1`,
+-- sends this to the Grobner basis module,
+-- and we can prove equalities modulo that relation!
+example : (cos x + sin x)^2 = 2 * cos x * sin x + 1 := by grind
+
+-- `grind` notices that the two arguments of `f` are equal,
+-- and hence the function applications are too.
+example (f : ℝ → ℕ) : f ((cos x + sin x)^2) = f (2 * cos x * sin x + 1) := by grind
+
+-- After that, we can use basic modularity conditions:
+-- this reduces to `4 * x ≠ 2 + x` for some `x : ℕ`
+example (f : ℝ → ℕ) : 4 * f ((cos x + sin x)^2) ≠ 2 + f (2 * cos x * sin x + 1) := by
+  grind
+
+-- A bit of case splitting is also fine.
+-- If `max = 3`, then `f _ = 0`, and we're done.
+-- Otherwise, the previous argument applies.
+
+variable {k V: Type} [Field k][AddCommGroup V][Module k V]
+
+example {I: Type} (i j: I) (W: I → Submodule k V) (h: i=j) (wi: W i) (wj: W j): True := by
+  have: W i = W j := sorry
+  have: wi = h ▸ wj := by
+    congr
+    sorry
+  sorry
