9a03e67c7a556def9332789fc50188a87157a0c0
George McNinch <gmcninch@gmail.com>
2025-07-18 09:04:38 -0400

    fri morn

--- VERSEIM2025/george/poly_module.lean
+++ VERSEIM2025/george/poly_module.lean
@@ -9,32 +9,45 @@ noncomputable section
 
 variable {R M: Type*} [CommRing R] [AddCommGroup M] [Module R M]
 
-variable {N:Type*} [AddCommGroup N] [Module R[X] N] [Module R N] [ IsScalarTower R R[X] N]
+variable {N:Type*} [AddCommGroup N] [Module R[X] N] [Module R N] 
+                   [IsScalarTower R R[X] N]
 
 def poly_map (f:M →ₗ[R] N) : PolynomialModule R M →ₗ[R[X]] N where
-  toFun := by
-    intro v
-    unfold PolynomialModule at v
-    exact Finsupp.sum v (fun (n:ℕ) _ => (Polynomial.monomial n (1:R)) • (f (v n)))
+  toFun :=  fun v => 
+    v.sum  fun (n:ℕ) _ => (Polynomial.monomial n (1:R)) • f (v n)
   map_add' := by 
     intro x y
-    sorry
+    have (x y : PolynomialModule R M) (n:ℕ): f ((x + y) n) = f (x n) + f (y n) := by
+      rw [PolynomialModule.add_apply R x y n]
+      simp
+    rw [ this x y ]
+    --sorry
   map_smul' := by
     intro m x
     sorry
 
--- inclusion of `M` into `PolynomialModule R M` is an R-module homomorphism
+example (f:ℕ →₀ ℤ) (g:ℕ →₀ ℤ) (h:(n:ℕ) → f n = g n) :
+  Finsupp.sum f (fun n _ => f n) = Finsupp.sum g (fun n _ => g n)  := by 
+  rw [h]
 
+
+
+-- inclusion of `M` into `PolynomialModule R M` is an R-module
+-- homomorphism it is important to use PolynomialModule.single rather
+-- than FinSupp.single here, to get easy proofs for map_add and
+-- map_smul
 def const_incl : M →ₗ[R] PolynomialModule R M where
-  toFun := fun m => Finsupp.single 0 m
-  map_add' := by simp
-  map_smul' := by sorry
-
+  toFun := PolynomialModule.single R 0
+  map_add' :=  by simp only [ map_add, implies_true ]
+  map_smul' := PolynomialModule.single_smul R 0 
 
 def poly_mod_equiv_tensor_product  : PolynomialModule R M ≃ₗ[R[X]] R[X] ⊗[R] M  where
   toFun := (poly_map const_incl).toFun
   map_add' := (poly_map const_incl).map_add
-  map_smul' := (poly_map const_incl).map_smul
+  map_smul' := by
+    intro f m
+    simp
+    (poly_map const_incl).map_smul 
   invFun := by sorry
   left_inv := by sorry
   right_inv := by sorry
