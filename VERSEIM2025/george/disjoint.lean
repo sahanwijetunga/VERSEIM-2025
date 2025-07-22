0ffe31deb1ebf753408774d7f906955714796909
George McNinch <gmcninch@gmail.com>
2025-07-22 16:24:30 -0400

    linear combination

--- VERSEIM2025/george/disjoint.lean
+++ VERSEIM2025/george/disjoint.lean
@@ -1,10 +1,12 @@
 import Mathlib.Tactic
 
 lemma not_left_in_right {s₁ s₂ : Type} (i : s₁ ⊕ s₂) (h : ¬ ∃ y:s₁, i = Sum.inl y) 
-  [DecidableEq s₁] [DecidableEq s₂] : ∃ z:s₂, i = Sum.inr z := by 
+  : ∃ z:s₂, i = Sum.inr z := by 
   match i with 
   | Sum.inr x => use x
   | Sum.inl y => 
      exfalso
      apply h
      use y
+
+  
--- VERSEIM2025/george/lincomb.lean
+++ VERSEIM2025/george/lincomb.lean
@@ -1,36 +1,18 @@
 import Mathlib.Tactic
 
-variable (k : Type) [ Field k ]
-variable (V : Type) [ AddCommGroup V ] [ Module k V ]
+variable {k : Type} [ Field k ]
+variable {V : Type} [ AddCommGroup V ] [ Module k V ]
 
-variable (ι:Type) [Fintype ι]
+variable {ι:Type} [Fintype ι]
 
-variable (b:Basis ι k V)
+variable {b:Basis ι k V}
 
--- example {v:V} (s:ι → k) (h: s = b.repr v) : (Finsupp.linearCombination k b) ↑s = v := by
---   rw [h]
---   apply Basis.linearCombination_repr 
-
-
-noncomputable
-example (s:ι→k) : ι →₀ k := by 
-  apply Finsupp.onFinset ⊤ s  
-  simp
-  
-example (v:V) : ι → k := b.repr v
-
-
--- example (s:ι →₀ k) : Finsupp.linearCombination k b s = ∑ (i:ι), (s i) • (b i) := by
---   apply Finsupp.linearCombination_onFinset k b (hf := by simp)
-
-
-lemma myrepr (v:V) : (Fintype.linearCombination k b) (b.repr v) = v := by 
-  nth_rewrite 2 [ ←Basis.linearCombination_repr b v ]
+lemma fintype_linear_combination_repr (v:V) : (Fintype.linearCombination k b) (b.repr v) = v := by 
+  apply Eq.trans _ (Basis.linearCombination_repr b v)
   rw [ Fintype.linearCombination_apply ]
-  let s : ι →₀ k := by 
-     apply  Finsupp.onFinset ⊤ (b.repr v) 
-     simp
-  
+  rw [ Finsupp.linearCombination_apply ]
+  rw [ Finsupp.sum_fintype ]
+  simp
 
 
      
