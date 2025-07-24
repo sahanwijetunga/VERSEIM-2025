b27838128351b52d2f735bb79fe560a58f512806
George McNinch <gmcninch@gmail.com>
2025-07-24 10:31:15 -0400

    xyz

--- VERSEIM2025/george/find_hyp.lean
+++ VERSEIM2025/george/find_hyp.lean
@@ -10,8 +10,21 @@ variable {b:Basis ι k V}
 open LinearMap
 open LinearMap (BilinForm)
 
-variable (β:BilinForm k V)
+variable {β:BilinForm k V} { h : IsRefl β } 
 
-structure is_hyp_two (W:Submodule k V)  where
+def fun_two {X:Type*} (x y:X) : Fin 2 → X :=
+  fun i => match i with
+    | 0 => x
+    | 1 => y
+
+structure hyperbolic_two_space (β:BilinForm k V) { h : IsRefl β } 
+  (W:Submodule k V)  where
+  e : V  
+  f : V
+  isotropic : β e e = 0 ∧ β f f = 0
+  nondeg : β e f = 1
+  lin_indep : LinearIndependent k (fun_two e f)
+  span : W = Submodule.span k { e , f }
   
   
+lemma hyp_is_nondeg (W:Submodule k V) (hyp : hyperbolic_two_space β W) : Nondegenerate β := by sorry
