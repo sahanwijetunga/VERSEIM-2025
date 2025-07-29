dfc36f0d91c297983b57debe88d1f0d67455c8a3
sahanwijetunga <99368343+sahanwijetunga@users.noreply.github.com>
2025-07-29 11:18:13 -0700

    Update ReflexiveBilinearForms.lean

--- VERSEIM2025/Forms/ReflexiveBilinearForms.lean
+++ VERSEIM2025/Forms/ReflexiveBilinearForms.lean
@@ -28,28 +28,28 @@ open BilinearForms -- This is the namespace in VERSEIM2025.Forms.Hyperbolic.Bili
 open LinearMap.BilinForm
 open LinearMap (BilinForm)
 
-variable {k V: Type*} [AddCommGroup V][Field k][Module k V]
+variable {F V: Type*} [AddCommGroup V][Field F][Module F V]
 
-def radForm (B: BilinForm k V) : Submodule k V where
+def radForm (B: BilinForm F V) : Submodule F V where
   carrier := {v | ∀ w, B v w = 0}
   add_mem' := by simp_all
   zero_mem' := by simp_all
   smul_mem' := by simp_all
 
-def radForm' (B: BilinForm k V) : Submodule k V where
+def radForm' (B: BilinForm F V) : Submodule F V where
   carrier := {v | ∀ w, B w v = 0}
   add_mem' := by simp_all
   zero_mem' := by simp_all
   smul_mem' := by simp_all
 
-theorem radForm'_eq_radForm_flip (B: BilinForm k V) :
+theorem radForm'_eq_radForm_flip (B: BilinForm F V) :
   radForm' B = radForm B.flip := by simp_all[radForm,radForm']
 
-theorem radForm_eq_radForm'_flip (B: BilinForm k V) :
+theorem radForm_eq_radForm'_flip (B: BilinForm F V) :
   radForm B = radForm' B.flip := by simp_all[radForm,radForm']
 
 @[simp]
-theorem flip_orthog_eq_orthog (B: BilinForm k V) (hr: B.IsRefl) (W: Submodule k V):
+theorem flip_orthog_eq_orthog (B: BilinForm F V) (hr: B.IsRefl) (W: Submodule F V):
   B.flip.orthogonal W = B.orthogonal W := by
   ext x
   constructor
@@ -58,57 +58,57 @@ theorem flip_orthog_eq_orthog (B: BilinForm k V) (hr: B.IsRefl) (W: Submodule k 
   . intro h
     exact fun n a ↦ hr n x (h n a)
 
-theorem radForm_eq_flip_orthogonal_top (B: BilinForm k V):
+theorem radForm_eq_flip_orthogonal_top (B: BilinForm F V):
   radForm B = B.flip.orthogonal ⊤ := by
     ext x
     simp_all[BilinForm.IsOrtho,radForm]
 
-theorem radForm'_eq_orthogonal_top (B: BilinForm k V):
+theorem radForm'_eq_orthogonal_top (B: BilinForm F V):
   radForm' B = B.orthogonal ⊤ := by
     ext x
     simp_all[BilinForm.IsOrtho,radForm']
 
-theorem radForm_eq_orthogonal_top (B: BilinForm k V) (hr: B.IsRefl):
+theorem radForm_eq_orthogonal_top (B: BilinForm F V) (hr: B.IsRefl):
   radForm B = B.orthogonal ⊤ := by
     rw[<- flip_orthog_eq_orthog B hr]
     exact radForm_eq_flip_orthogonal_top B
 
-theorem radForm'_eq_flip_orthogonal_top (B: BilinForm k V) (hr: B.IsRefl):
+theorem radForm'_eq_flip_orthogonal_top (B: BilinForm F V) (hr: B.IsRefl):
   radForm' B = B.flip.orthogonal ⊤ := by
     rw[flip_orthog_eq_orthog B hr]
     exact radForm'_eq_orthogonal_top B
 
 @[simp]
-theorem radForm'_eq_radForm (B: BilinForm k V) (hr: B.IsRefl) :
+theorem radForm'_eq_radForm (B: BilinForm F V) (hr: B.IsRefl) :
   radForm' B = radForm B := by
   rw[radForm'_eq_flip_orthogonal_top _ hr, radForm_eq_flip_orthogonal_top]
 
 
-theorem form_on_radForm_eq_zero (B: BilinForm k V):
+theorem form_on_radForm_eq_zero (B: BilinForm F V):
   B.restrict (radForm B) = 0 := by
     ext ⟨x, hx⟩ _
     apply hx
 
-theorem form_on_radForm'_eq_zero (B: BilinForm k V):
+theorem form_on_radForm'_eq_zero (B: BilinForm F V):
   B.restrict (radForm' B) = 0 := by
     ext _ ⟨y, hy⟩
     apply hy
 
 -- Name could be improved
-theorem orthog_direct_sum_of_radForm_isCompl (B: BilinForm k V)
-  (W: Submodule k V) (hW: IsCompl (radForm B) W): is_orthog_direct_sum_weak B (radForm B) W := by
+theorem orthog_direct_sum_of_radForm_isCompl (B: BilinForm F V)
+  (W: Submodule F V) (hW: IsCompl (radForm B) W): is_orthog_direct_sum_weak B (radForm B) W := by
   constructor
   . apply (direct_sum_iff_iscompl _ _).mpr hW
   . rintro a ha b hb
     exact ha b
 
-theorem Nondegenerate_of_isCompl_of_radForm (B: BilinForm k V) (hr: B.IsRefl)
-  (W: Submodule k V) (hW: IsCompl (radForm B) W): (B.restrict W).Nondegenerate := by
+theorem Nondegenerate_of_isCompl_of_radForm (B: BilinForm F V) (hr: B.IsRefl)
+  (W: Submodule F V) (hW: IsCompl (radForm B) W): (B.restrict W).Nondegenerate := by
     intro ⟨a, ha⟩ h'
     suffices a=0 from (Submodule.mk_eq_zero W ha).mpr this
     dsimp at h'
     have: W ⊓ radForm B = ⊥ := IsCompl.inf_eq_bot (_root_.id (IsCompl.symm hW))
-    show a ∈ (⊥: Submodule k V)
+    show a ∈ (⊥: Submodule F V)
     rw[<- this]
     constructor; . exact ha
     show ∀ b, B a b = 0
@@ -135,8 +135,8 @@ theorem Nondegenerate_of_isCompl_of_radForm (B: BilinForm k V) (hr: B.IsRefl)
 -- however the choice is not canonical so it may be more difficult to construct.
 -- (Note: any such definition will have to be noncomputable)
 -- (Note: is_orthog_direct_sum and is_orthog_direct_sum' are equivalent given Reflexivity)
-theorem sum_radForm_nondegenerate (B: BilinForm k V) (hr: B.IsRefl):
-  ∃ (W: Submodule k V), (is_orthog_direct_sum B (radForm B) W ∧ (B.restrict W).Nondegenerate) := by
+theorem sum_radForm_nondegenerate (B: BilinForm F V) (hr: B.IsRefl):
+  ∃ (W: Submodule F V), (is_orthog_direct_sum B (radForm B) W ∧ (B.restrict W).Nondegenerate) := by
     have ⟨W, h⟩ := Submodule.exists_isCompl (radForm B)
     use W
     constructor
@@ -144,24 +144,95 @@ theorem sum_radForm_nondegenerate (B: BilinForm k V) (hr: B.IsRefl):
       exact orthog_direct_sum_of_radForm_isCompl B W h
     exact Nondegenerate_of_isCompl_of_radForm B hr W h
 
-/-
-  Proof outline for `refl_is_alt_or_symm`:
-  Let B be a reflexive bilinear form over V. Suppose B is not alternating. Then there exists
-  v ∈ V with B(v,v) ≠ 0. Then,
-    V ≃ (Span v) ⊕ (Span v)ᗮ as bilinear form spaces (note B reflexive)
-  If (Span v)ᗮ is symmetric, then as (Span v) is trivially symmetric we see V is symmetric.
+theorem symmetry_extend {B: BilinForm F V} {v: V} (hr: IsRefl B)
+   (h: ∀ x y, B x v = 0 → B y v = 0 → B x y = B y x) (hv: B v v ≠ 0): IsSymm B := by
+  have: ∀ w, ∃ (c: F), ∃ u, B u v = 0 ∧ w = c • v + u := by
+    intro w
+    have: w ∈ Submodule.span F {v} ⊔ B.orthogonal (Submodule.span F {v}) := by
+      rw[LinearMap.BilinForm.span_singleton_sup_orthogonal_eq_top hv]
+      exact trivial
+    rw[Submodule.mem_sup] at this
+    obtain ⟨y, hy, z, hz, hyz⟩ := this
+    rw[Submodule.mem_span_singleton] at hy
+    obtain ⟨c, hc⟩ := hy
+    use c, z
+    constructor
+    . rw[LinearMap.BilinForm.mem_orthogonal_iff] at hz
+      apply hr
+      apply hz
+      exact Submodule.mem_span_singleton_self v
+    rw[<- hyz, hc]
+  intro x y; simp only [RingHom.id_apply]
+  obtain ⟨cx, ux, huxv, hx⟩ := this x
+  obtain ⟨cy, uy, huyv, hy⟩ := this y
+  have huxy: B ux uy = B uy ux := h ux uy huxv huyv
+  rw[hx, hy]
+  repeat rw[add_left]
+  repeat rw[add_right]
+  repeat rw[smul_left]
+  repeat rw[smul_right]
+  rw[huyv, huxv, huxy, hr _ _ huyv, hr _ _ huxv]
+  ring
 
-  Otherwise, take w₁,w₂ ∈ (Span v)ᗮ with B(w₁,w₂) ≠ B(w₂,w₁). Let c := B(v,v) and
-  WLOG B(w₁,w₂) = -c² from scaling. Thus,
-    B(w₁+v,w₂+v) = 0
-    B(w₂+v,w₁+v) = B(w₁+w₂)+c^2 = B(w₁+w₂)-B(w₁,w₂)
-  hence B(w₁,w₂) = B(w₂,w₁) a contradiction.
--/
+theorem refl_is_alt_or_symm {B: BilinForm F V} (h: IsRefl B) [FiniteDimensional F V] :
+  IsAlt B ∨ IsSymm B := by
+  by_cases ha:IsAlt B
+  . left; exact ha
+  have: ∃ v, B v v ≠ 0 := by
+    unfold IsAlt LinearMap.IsAlt at ha
+    push_neg at ha
+    exact ha
+  obtain ⟨v, hv⟩ := this
+  by_cases hv': ∀ x y, B x v = 0 → B y v = 0 → B x y = B y x
+  . right; exact  symmetry_extend h hv' hv
+  have: ∃ x y, B x v = 0 ∧ B y v = 0 ∧ B x y ≠ B y x := by
+    push_neg at hv'
+    exact hv'
+  have: ∃ x y, B x v = 0 ∧ B y v = 0 ∧ B x y ≠ B y x ∧ B x y = -B v v := by
+    obtain ⟨x,y, hxv, hyv, hxy⟩ := this
+    let d: F := -(B v v)* (B x y)⁻¹
+    use d • x, y
+    have hxy_neqzero: B x y ≠ 0 := by
+      intro hxy_zero
+      have: B x y = B y x := by
+        rw[h _ _ hxy_zero, hxy_zero]
+      exact hxy this
+    constructor
+    . rw [smul_left]
+      simp[hxv]
+    constructor
+    . exact hyv
+    constructor
+    . intro hxyc
+      apply hxy
+      rw [smul_left, smul_right] at hxyc
+      have: d ≠ 0 := by
+        unfold d
+        field_simp[hv]
+      simp_all
+    . rw [smul_left]
+      unfold d
+      rw[mul_assoc, inv_mul_cancel₀ hxy_neqzero]
+      ring
 
-theorem refl_is_alt_or_symm {B: BilinForm k V} (h: IsRefl B) [FiniteDimensional k V] :
-  IsAlt B ∨ IsSymm B := sorry
+  obtain ⟨x, y, hxv ,hyv, hxy, hxyc⟩ := this
+  have h1: B (y+v) (x+v) = 0 := by
+    apply h
+    rw [@add_right, add_left, add_left]
+    rw[hxv, h _ _ hyv, hxyc]
+    ring_nf
+  have h2: B (y+v) (x+v) = B y x - B x y := by
+    rw [@add_right, add_left, add_left, h _ _ hxv, hyv, hxyc]
+    ring
+  have h3: B x y = B y x := by
+    have: B y x - B x y = 0 := by
+      rw[<- h1,h2]
+    exact (sub_eq_zero.mp this).symm
+  exfalso
+  exact hxy h3
 
-def quot_form {B: BilinForm k V} (hb: B.IsRefl): BilinForm k (V ⧸ (radForm B)) := sorry
 
-theorem reflexive_quotient_radForm_nondegenerate (B: BilinForm k V) (hr: B.IsRefl):
+def quot_form {B: BilinForm F V} (hb: B.IsRefl): BilinForm F (V ⧸ (radForm B)) := sorry
+
+theorem reflexive_quotient_radForm_nondegenerate (B: BilinForm F V) (hr: B.IsRefl):
    (quot_form hr).Nondegenerate := sorry
