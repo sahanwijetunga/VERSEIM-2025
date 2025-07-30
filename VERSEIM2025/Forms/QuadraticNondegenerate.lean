868860c05291446c20f1b0255c005ed39003f63a
sahanwijetunga <99368343+sahanwijetunga@users.noreply.github.com>
2025-07-30 15:03:36 -0700

    .

--- VERSEIM2025/Forms/QuadraticNondegenerate.lean
+++ VERSEIM2025/Forms/QuadraticNondegenerate.lean
@@ -19,7 +19,7 @@ import Mathlib.RingTheory.Flat.Basic
 
 import VERSEIM2025.PolynomialModuleDegree.Misc
 import VERSEIM2025.Forms.RationalFunctionFields.PolynomialModule
-import VERSEIM2025.Forms.ReflexiveBilinearForms
+import VERSEIM2025.Forms.Reflexive.Radical
 
 namespace QuadraticForm
 
@@ -32,8 +32,7 @@ variable {R M: Type*} [CommRing R] [AddCommGroup M] [Module R M]
 
 def rad (Q: QuadraticForm R M): Submodule R M := radForm Q.polarBilin
 
-theorem rad_iff (Q: QuadraticForm R M) (x: M): x ∈ Q.rad ↔ ∀ y, Q.polarBilin x y = 0 := by
-  simp[rad,radForm]
+theorem rad_iff (Q: QuadraticForm R M) (x: M): x ∈ Q.rad ↔ ∀ y, Q.polarBilin x y = 0 := by rfl
 
 theorem rad_iff' (Q: QuadraticForm R M) (x: M): x ∈ Q.rad ↔ ∀ y, Q.polarBilin y x = 0 := by
   simp[rad,radForm,QuadraticMap.polar]
@@ -43,17 +42,48 @@ theorem rad_iff' (Q: QuadraticForm R M) (x: M): x ∈ Q.rad ↔ ∀ y, Q.polarBi
     rw[<- h y, add_comm]
     abel_nf
 
+@[simp]
 theorem in_rad_apply (Q: QuadraticForm R M) {x: M} (hx: x ∈ Q.rad) (y: M): Q.polarBilin x y = 0 := by
   exact (rad_iff Q x).mp hx y
 
+@[simp]
 theorem in_rad_apply' (Q: QuadraticForm R M) {x: M} (hx: x ∈ Q.rad) (y: M): Q.polarBilin y x= 0:= by
   exact (rad_iff' Q x).mp hx y
 
-theorem in_rad_apply'' (Q: QuadraticForm R M) {x: M} (hx: x ∈ Q.rad) (y: M): Q (x+y) = Q y := sorry
 
-theorem in_rad_apply''' (Q: QuadraticForm R M) {x: M} (hx: x ∈ Q.rad) (y: M): Q (y+x) = Q y := sorry
+theorem in_rad_apply'' (Q: QuadraticForm R M) {x: M} (hx: x ∈ Q.rad) (y: M) [Invertible (2:R)]: Q (x+y) = Q y := by
+  rw[QuadraticMap.map_add Q x y]
+  rw[<- QuadraticMap.polarBilin_apply_apply]
+  rw[in_rad_apply Q hx y]
+  simp
+  suffices h:2 • (Q x) = 0 from ?_
+  . have: 2 • (Q x) = (2:R) * Q x := by exact Eq.symm (ofNat_smul_eq_nsmul R 2 (Q x))
+    rw[this] at h
+    calc
+      Q x = (1:R) * (Q x) := by rw[one_mul]
+      _ = (⅟ (2: R) * (2: R)) * (Q x) := by
+        rw [Invertible.invOf_mul_self]
+      _ = ⅟ (2: R) * ((2: R) * Q x) := by
+        rw[mul_assoc]
+      _ = 0 := by
+        rw[h, mul_zero]
+  rw[<- QuadraticMap.polar_self]
+  exact hx x
 
-noncomputable def quotient_rad (Q: QuadraticForm R M): QuadraticForm R (M ⧸ Q.rad) where
+theorem in_rad_apply''' (Q: QuadraticForm R M) {x: M} (hx: x ∈ Q.rad) (y: M) [Invertible (2:R)]: Q (y+x) = Q y := by
+  rw[add_comm]
+  exact in_rad_apply'' Q hx y
+
+
+theorem polarBilin_isSymm (Q: QuadraticForm R M): Q.polarBilin.IsSymm := by
+  intro x y
+  simp only [QuadraticMap.polarBilin_apply_apply, RingHom.id_apply]
+  exact QuadraticMap.polar_comm (⇑Q) x y
+
+example (a b c: M) (h: a-b=c): a=b+c := by
+  exact Eq.symm (add_eq_of_eq_sub' (id (Eq.symm h)))
+
+noncomputable def quotient_rad (Q: QuadraticForm R M) [Invertible (2:R)]: QuadraticForm R (M ⧸ Q.rad) where
   toFun := by
     apply Quotient.lift Q
     intro a b hab
@@ -68,20 +98,59 @@ noncomputable def quotient_rad (Q: QuadraticForm R M): QuadraticForm R (M ⧸ Q.
     intro a
     intro v
     induction' v using Submodule.Quotient.induction_on with v
-    simp
-    sorry
-  exists_companion' := sorry
+    rw[<- Submodule.Quotient.mk_smul]
+    simp[Submodule.Quotient.mk]
+    rw[<- smul_eq_mul _ (Q v)]
+    exact QuadraticMap.map_smul Q a v
+  exists_companion' := by -- why does rewriting not work?
+    use quot_form (B := Q.polarBilin) (LinearMap.BilinForm.IsSymm.isRefl Q.polarBilin_isSymm)
+    intro x y
+    induction' x using Submodule.Quotient.induction_on with x
+    induction' y using Submodule.Quotient.induction_on with y
+    rw[<- Submodule.Quotient.mk_add]
+    simp[Submodule.Quotient.mk, QuadraticMap.polar, <- Submodule.Quotient.mk_add]
+    have: quot_form (B := Q.polarBilin) (LinearMap.BilinForm.IsSymm.isRefl (polarBilin_isSymm Q))
+      (Submodule.Quotient.mk x) (Submodule.Quotient.mk y) = Q.polarBilin x y := by
+      exact quot_form_apply (B := Q.polarBilin) (LinearMap.BilinForm.IsSymm.isRefl (polarBilin_isSymm Q)) (v := x) (w := y)
+    symm
+    apply add_eq_of_eq_sub'
+    rw[sub_add_eq_sub_sub]
+    simpa [Submodule.Quotient.mk, QuadraticMap.polarBilin_apply_apply,QuadraticMap.polar] using this
 
-theorem quotient_rad_polarBilin (Q: QuadraticForm R M): Q.quotient_rad.polarBilin=  quot_form (B := Q.polarBilin) sorry := by
-  simp[quotient_rad]
-  sorry
+theorem quotient_rad_apply (Q: QuadraticForm R M) (x: M)[Invertible (2:R)]: Q.quotient_rad (Submodule.Quotient.mk  x) = Q x := rfl
 
 
-theorem quotient_rad_nondegenerate (Q: QuadraticForm R M): LinearMap.BilinForm.Nondegenerate Q.quotient_rad.polarBilin := by
+theorem quotient_rad_polarBilin (Q: QuadraticForm R M)[Invertible (2:R)]: Q.quotient_rad.polarBilin =
+  quot_form (B := Q.polarBilin) (LinearMap.BilinForm.IsSymm.isRefl Q.polarBilin_isSymm) := by
+  ext x y
+  simp[QuadraticMap.polar,quotient_rad_apply, <- Submodule.Quotient.mk_add]
+  have: quot_form (B := Q.polarBilin) (LinearMap.BilinForm.IsSymm.isRefl (polarBilin_isSymm Q))
+    (Submodule.Quotient.mk x) (Submodule.Quotient.mk y) = Q.polarBilin x y := quot_form_apply (B := Q.polarBilin) Q.polarBilin_isSymm.isRefl (v := x) (w := y)
+  apply Eq.trans (h₂ := this.symm)
+  simp[QuadraticMap.polar]
+
+
+theorem quotient_rad_nondegenerate (Q: QuadraticForm R M)[Invertible (2:R)]: LinearMap.BilinForm.Nondegenerate Q.quotient_rad.polarBilin := by
   rw[quotient_rad_polarBilin]
   exact reflexive_quotient_radForm_nondegenerate _ _
 
-theorem range_quotient_rad_eq_range (Q: QuadraticForm R M): Set.range Q.quotient_rad = Set.range Q := sorry
+-- this is amazing to me that rfl works! (finally lean working as I want it)
+theorem quotient_rad_comp (Q: QuadraticForm R M)[Invertible (2:R)]: Q.quotient_rad.comp Q.rad.mkQ = Q := rfl
+
+theorem range_comp_eq {N: Type*} [AddCommGroup N] [Module R N] (Q : QuadraticForm R M)
+  (f : N →ₗ[R] M) (hf: Function.Surjective f):
+  Set.range (Q.comp f) = Set.range Q := by
+  ext x
+  constructor
+  . rintro ⟨y, rfl⟩
+    exact Set.mem_range_self (f y)
+  . rintro ⟨y, rfl⟩
+    obtain ⟨z, rfl⟩ := hf y
+    exact Set.mem_range_self z
+
+theorem range_quotient_rad_eq_range (Q: QuadraticForm R M)[Invertible (2:R)]: Set.range Q.quotient_rad = Set.range Q := by
+  rw[<- range_comp_eq Q.quotient_rad Q.rad.mkQ (Submodule.mkQ_surjective Q.rad)]
+  rw[quotient_rad_comp]
 
 theorem range_baseChange_quotient_rad_eq_range_baseChange [Invertible (2:R)] (A: Type*) [CommRing A] [Algebra R A]
   (Q: QuadraticForm R M): Set.range (Q.baseChange A) = Set.range (Q.quotient_rad.baseChange A) :=
---
+++ VERSEIM2025/Forms/Reflexive/Radical.lean
@@ -0,0 +1,187 @@
+--------------------------------------------------------------------------------
+/-
+Copyright (c) 2025 Clea Bergsman, Katherine Buesing, George McNinch, Sahan Wijetunga. All rights reserved.
+
+Released under the Apache 2.0 license as described in the file LICENSE.
+
+VERSEIM-2025 REU @ Tufts University
+-/
+
+import Mathlib.Tactic
+import Mathlib.LinearAlgebra.BilinearForm.Orthogonal
+import VERSEIM2025.Forms.BilinearForms
+
+/-
+  Major results (Completed)
+  - A reflexive bilinear form can be written as a direct sum of 0
+    and a nondegenerate bilinear form
+    - `reflexive_sum_radForm_nondegenerate` (and `form_on_radForm_eq_zero`)
+  - The quotient of a reflexive bilinear form by its radical is nondegenerate
+    - `reflexive_quotient_radForm_nondegenerate`
+
+  TODO: Clean up definitions (radForm/radForm' excessive amount of theorems?)
+-/
+
+open BilinearForms -- This is the namespace in VERSEIM2025.Forms.Hyperbolic.BilinearForms
+open LinearMap.BilinForm
+open LinearMap (BilinForm)
+
+variable {R M: Type*} [AddCommGroup M][CommRing R] [Module R M]
+variable {F V: Type*} [Field F] [AddCommGroup V] [Module F V]
+
+def radForm (B: BilinForm R M) : Submodule R M where
+  carrier := {v | ∀ w, B v w = 0}
+  add_mem' := by simp_all
+  zero_mem' := by simp_all
+  smul_mem' := by simp_all
+
+def radForm' (B: BilinForm R M) : Submodule R M where
+  carrier := {v | ∀ w, B w v = 0}
+  add_mem' := by simp_all
+  zero_mem' := by simp_all
+  smul_mem' := by simp_all
+
+theorem radForm_eq_kernel (B: BilinForm R M): radForm B = LinearMap.ker B := by
+  ext x; simp[radForm]
+  constructor
+  . intro h
+    ext w
+    simp_all [LinearMap.zero_apply]
+  . simp_all [LinearMap.zero_apply]
+
+theorem radForm'_eq_radForm_flip (B: BilinForm R M) :
+  radForm' B = radForm B.flip := by simp_all[radForm,radForm']
+
+theorem radForm_eq_radForm'_flip (B: BilinForm R M) :
+  radForm B = radForm' B.flip := by simp_all[radForm,radForm']
+
+@[simp]
+theorem flip_orthog_eq_orthog (B: BilinForm R M) (hr: B.IsRefl) (W: Submodule R M):
+  B.flip.orthogonal W = B.orthogonal W := by
+  ext x
+  constructor
+  . intro h
+    exact fun n a ↦ hr x n (h n a)
+  . intro h
+    exact fun n a ↦ hr n x (h n a)
+
+theorem radForm_eq_flip_orthogonal_top (B: BilinForm R M):
+  radForm B = B.flip.orthogonal ⊤ := by
+    ext x
+    simp_all[BilinForm.IsOrtho,radForm]
+
+theorem radForm'_eq_orthogonal_top (B: BilinForm R M):
+  radForm' B = B.orthogonal ⊤ := by
+    ext x
+    simp_all[BilinForm.IsOrtho,radForm']
+
+theorem radForm_eq_orthogonal_top (B: BilinForm R M) (hr: B.IsRefl):
+  radForm B = B.orthogonal ⊤ := by
+    rw[<- flip_orthog_eq_orthog B hr]
+    exact radForm_eq_flip_orthogonal_top B
+
+theorem radForm'_eq_flip_orthogonal_top (B: BilinForm R M) (hr: B.IsRefl):
+  radForm' B = B.flip.orthogonal ⊤ := by
+    rw[flip_orthog_eq_orthog B hr]
+    exact radForm'_eq_orthogonal_top B
+
+@[simp]
+theorem radForm'_eq_radForm (B: BilinForm R M) (hr: B.IsRefl) :
+  radForm' B = radForm B := by
+  rw[radForm'_eq_flip_orthogonal_top _ hr, radForm_eq_flip_orthogonal_top]
+
+
+theorem form_on_radForm_eq_zero (B: BilinForm R M):
+  B.restrict (radForm B) = 0 := by
+    ext ⟨x, hx⟩ _
+    apply hx
+
+theorem form_on_radForm'_eq_zero (B: BilinForm R M):
+  B.restrict (radForm' B) = 0 := by
+    ext _ ⟨y, hy⟩
+    apply hy
+
+-- Name could be improved
+theorem orthog_direct_sum_of_radForm_isCompl (B: BilinForm F V)
+  (W: Submodule F V) (hW: IsCompl (radForm B) W): is_orthog_direct_sum_weak B (radForm B) W := by
+  constructor
+  . apply (direct_sum_iff_iscompl _ _).mpr hW
+  . rintro a ha b hb
+    exact ha b
+
+theorem Nondegenerate_of_isCompl_of_radForm (B: BilinForm R M) (hr: B.IsRefl)
+  (W: Submodule R M) (hW: IsCompl (radForm B) W): (B.restrict W).Nondegenerate := by
+    intro ⟨a, ha⟩ h'
+    suffices a=0 from (Submodule.mk_eq_zero W ha).mpr this
+    dsimp at h'
+    have: W ⊓ radForm B = ⊥ := IsCompl.inf_eq_bot (_root_.id (IsCompl.symm hW))
+    show a ∈ (⊥: Submodule R M)
+    rw[<- this]
+    constructor; . exact ha
+    show ∀ b, B a b = 0
+    intro b
+    have: ∃ y ∈ W, ∃ z ∈ (radForm B), y + z = b := by
+      have: W ⊔ (radForm B)= ⊤ := IsCompl.sup_eq_top (_root_.id (IsCompl.symm hW))
+      rw[<- Submodule.mem_sup]
+      rw[this]
+      trivial
+    have ⟨y,hy,z,hz,hyz⟩ := this
+    rw[<- hyz]
+    calc
+      (B a (y + z)) = B a y + B a z := by
+        exact add_right a y z
+      _ = 0 + 0 := by
+        have: B a z = 0 := hr z a (hz a) -- Note: this is the only place we need reflexivity
+        rw[this]
+        have: B a y = 0 := by
+          exact h' ⟨y, hy⟩
+        rw[this]
+      _ = 0 := by rw[add_zero]
+
+
+-- Ideally one would separate out the submodule into a definition,
+-- however the choice is not canonical so it may be more difficult to construct.
+-- (Note: any such definition will have to be noncomputable)
+-- (Note: is_orthog_direct_sum and is_orthog_direct_sum' are equivalent given Reflexivity)
+theorem sum_radForm_nondegenerate  (B: BilinForm F V) (hr: B.IsRefl):
+  ∃ (W: Submodule F V), (is_orthog_direct_sum B (radForm B) W ∧ (B.restrict W).Nondegenerate) := by
+    have ⟨W, h⟩ := Submodule.exists_isCompl (radForm B)
+    use W
+    constructor
+    . refine is_orthog_direct_sum_of_is_orthog_direct_sum_weak_refl ?_ hr
+      exact orthog_direct_sum_of_radForm_isCompl B W h
+    exact Nondegenerate_of_isCompl_of_radForm B hr W h
+
+def quot_form {B: BilinForm R M} (hb: B.IsRefl): BilinForm R (M ⧸ (radForm B)) :=
+  (Submodule.liftQ _ (LinearMap.flip (Submodule.liftQ _ B (by rw[radForm_eq_kernel B]  )))
+  ( by
+    nth_rewrite 1[radForm_eq_kernel B]
+    intro v hv
+    ext w
+    apply hb
+    simp_all
+  )
+  ).flip
+
+
+def quot_form_apply {B: BilinForm R M} (hb: B.IsRefl) (v w: M):
+(quot_form hb) (Submodule.Quotient.mk v) (Submodule.Quotient.mk w) = B v w := by
+  simp[quot_form]
+
+def quot_form_apply' {B: BilinForm R M} (hb: B.IsRefl):
+  B = (quot_form hb).comp (radForm B).mkQ (radForm B).mkQ := by
+  ext v w
+  exact quot_form_apply hb v w
+
+theorem reflexive_quotient_radForm_nondegenerate (B: BilinForm R M) (hr: B.IsRefl):
+   (quot_form hr).Nondegenerate := by
+  intro v hv
+  induction' v using Submodule.Quotient.induction_on with v
+  suffices v ∈ radForm B from ?_
+  . simpa using this
+  have: ∀ w, B v w = 0 := by
+    intro w
+    have: ((quot_form hr) (Submodule.Quotient.mk v)) (Submodule.Quotient.mk w) = 0 := by
+      exact hv (Submodule.Quotient.mk w)
+    simpa using this
+  exact this
