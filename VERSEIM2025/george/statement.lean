df3996ace27738fbc727f8a2954e4d09e46d1147
George McNinch <gmcninch@gmail.com>
2025-07-30 16:32:15 -0400

    asdf

--- VERSEIM2025/george/statement.lean
+++ VERSEIM2025/george/statement.lean
@@ -8,7 +8,7 @@ variable  {k V₁ V₂ :Type} [Field k]
   [AddCommGroup V₁] [Module k V₁]
   [AddCommGroup V₂] [Module k V₂]
 
-structure equiv_of_spaces_with_form
+structure Isometries
   (β₁:BilinForm k V₁) 
   (β₂:BilinForm k V₂)
   where
@@ -16,12 +16,12 @@ structure equiv_of_spaces_with_form
     compat : ∀ (x y : V₁), β₁ x y = β₂ (equiv v₁) (equiv v₂) 
 
 notation:100 lhs:100 "≃[" field:100 "," lhb:100 "," rhb:100 "]" rhs:100 => 
-  equiv_of_spaces_with_form (k:= field) (V₁ := lhs) (V₂ := rhs) lhb rhb
+  Isometries (k:= field) (V₁ := lhs) (V₂ := rhs) lhb rhb
 
-theorem equiv_via_matrices {ι:Type} [Fintype ι] 
+theorem equiv_via_matrices {ι:Type} [Fintype ι] [DecidableEq ι]
   (β₁ : BilinForm k V₁)   (β₂ : BilinForm k V₂) 
   (b₁ : Basis ι k V₁)
-  :  V₁ ≃[k,β₁,β₂] V₂  ↔  ∃ b₂:Basis ι k V₂,
+  :  Nonempty (V₁ ≃[k,β₁,β₂] V₂)  ↔  ∃ b₂:Basis ι k V₂,
     (BilinForm.toMatrix b₁ β₁) =  (BilinForm.toMatrix b₂ β₂) := by sorry
 
 
@@ -30,3 +30,7 @@ noncomputable def alternate_iso {B₁: BilinForm k V₁} (B₂: BilinForm k V₂
   (hd₁: B₁.Nondegenerate) (hd₂: B₂.Nondegenerate) 
   [FiniteDimensional k V₁] [FiniteDimensional k V₂]
   (h: Module.finrank k V₁ = Module.finrank k V₂) : V₁ ≃[k,B₁,B₂] V₂ := by sorry
+
+
+theorem refl_is_alt_or_symm {B: BilinForm k V₁} (h: B.IsRefl) [FiniteDimensional k V₁] :
+    B.IsAlt ∨ B.IsSymm := by sorry
